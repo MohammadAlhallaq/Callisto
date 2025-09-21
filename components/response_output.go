27351f056065b637fec3564860a400d66b9b0033
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2025-09-21 18:36:15 +0300

    fix: disable response output

--- components/response_output.go
+++ components/response_output.go
@@ -9,6 +9,7 @@ func NewResponseView() *widget.Entry {
 	// INITIALIZE RESPONSE WIDGET
 	output := widget.NewMultiLineEntry()
 	output.SetPlaceHolder("Respones will appear here...")
+	output.Disable()
 	output.Wrapping = fyne.TextWrapWord
 	return output
 }
